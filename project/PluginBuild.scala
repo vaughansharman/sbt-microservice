0608d00c42cd9dfae4ee90805b15fcd11c9bdbb0
Duncan Crawford <duncan@tandemconsulting.eu>
2014-06-27 17:40:18 +0100

    increment to snapshot version

--- project/PluginBuild.scala
+++ project/PluginBuild.scala
@@ -19,7 +19,7 @@ import Keys._
 object PluginBuild extends Build {
 
   val pluginName = "sbt-microservice"
-  val pluginVersion = "3.1.0"
+  val pluginVersion = "3.2.0-SNAPSHOT"
 
   lazy val root = Project(pluginName, base = file("."), settings = Project.defaultSettings ++ Seq(
 
